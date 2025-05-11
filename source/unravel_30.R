307981294fbcf33b17402762c5c978e2ba406c9c
Danielle Navarro <djnavarro@protonmail.com>
2025-05-11 17:45:44 +1000

    stuff

---
+++ source/unravel_30.R
@@ -0,0 +1,96 @@
+
+# set up ------------------------------------------------------------------
+
+name    <- "unravel" 
+version <- 30
+
+# define common helper functions
+source(here::here("source", "common.R"), echo = FALSE)
+
+# import C++ functions
+cpp_file <- paste0(name, "_", tidy_int_string(version, width = 2), ".cpp")
+Rcpp::sourceCpp(here::here("source", cpp_file))
+
+# art generator -----------------------------------------------------------
+
+default_seeds <- function(version) {
+  0:99 + version * 100
+}
+
+colourise_data <- function(df, pal, px) {
+  df <- pal[df]
+  df <- matrix(df, px, px, byrow = TRUE)
+  return(df)
+}
+
+transform_data <- function(df, ncl) {
+  df <- rank(abs(df))
+  df <- df - min(df)
+  df <- df / max(df)
+  df <- as.integer(df * (ncl - 1)) + 1
+  return(df)
+}
+
+write_image <- function(df, fpath, px, bg) {
+  rs <- as.raster(df)
+  jpeg(
+    filename = fpath,
+    width = px,
+    height = px,
+    bg = bg 
+  )
+  op <- par(mar = c(0,0,0,0))
+  plot(rs)
+  dev.off()
+  par(op)
+}
+
+generate_data <- function(seed, iter, layers, px, zoom, alpha) {
+  set.seed(seed)
+  mat <- art_data(iter, layers, px, zoom)
+  if(runif(1) < .5) mat <- mat[px:1, ]
+  if(runif(1) < .5) mat <- mat[, px:1]
+  return(mat)
+}
+
+expand_palette <- function(shades, to = 1024L) {
+  (colorRampPalette(shades))(to)
+}
+
+generate_palette <- function(seed) {
+  set.seed(seed)
+  here::here("source", "palettes", "palette_04.csv") |> 
+    readr::read_csv(show_col_types = FALSE) |> 
+    dplyr::slice_sample(n = 1) |> 
+    unlist() |> 
+    sample() |> 
+    expand_palette(to = 1024L)
+}
+
+art_generator <- function(seed) {
+  
+  set.seed(seed)
+  
+  output <- output_path(name, version, seed, "jpg")
+  message("generating ", output)
+  
+  layers <- 10
+  iter   <- 1000 * 10^6
+  px     <- 2000  
+  zoom   <- 0.2
+
+  shades <- generate_palette(seed)
+
+  img <- generate_data(seed, iter, layers, px, zoom)
+  img <- transform_data(img, ncl = length(shades))
+  img <- colourise_data(img, pal = shades, px = px)
+  
+  write_image(img, output, px, shades[1])
+  
+}
+
+
+# make art ----------------------------------------------------------------
+
+seeds <- default_seeds(version)
+for(s in seeds) art_generator(s)
---
+++ source/unravel_30.cpp
@@ -0,0 +1,130 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+#include <iostream>
+using namespace std;
+
+// function to be called from R
+// [[Rcpp::export]]
+NumericMatrix art_data(int iter, int layers, int pixels, double zoom) {
+
+  // storage matrices
+  NumericMatrix image(pixels, pixels); 
+  NumericMatrix count(pixels, pixels);
+  NumericMatrix coeffs(9, layers);
+
+  // set coefficients
+  for(int i = 0; i < 9; i++) {
+    for(int j = 0; j < layers; j++) {
+      coeffs(i,j) = R::runif(-1,1) * 0.3;
+    }
+  }
+
+  // set image matrix to zeros
+  for(int r = 0; r < pixels; r++) {
+    for(int c = 0; c < pixels; c++) {
+      image(c, r) = 0;
+      count(c, r) = 0;
+    }
+  }
+  
+  // iterate
+  int layer;
+  int variant;
+
+  // indices for storing coordinates
+  int x_ind;
+  int y_ind;
+  
+  // values for current state
+  double x = 0;
+  double y = 0;
+  double z = 0;
+  double w = 0; 
+  
+  // values for previous state
+  double x_old = R::runif(-1, 1);
+  double y_old = R::runif(-1, 1);
+  double z_old = R::runif(-1, 1);
+  
+  // handy variables
+  double s;
+  double theta;
+  double u1;
+  double u2;
+  double u3;
+  double f;
+
+  // constants
+  const double pi = 3.1415926535;
+
+  // chaos game
+  for (int t = 1; t < iter; t++) {
+
+    layer = rand() % layers;  // which affine transform to use?
+    variant = rand() % 4;     // which variant function to use?
+
+    // coordinates after random transform
+    x = x_old + coeffs(0, layer) * x_old + coeffs(1, layer) * y_old + coeffs(2, layer);
+    y = y_old + coeffs(3, layer) * x_old + coeffs(4, layer) * y_old + coeffs(5, layer);
+    z = z_old + coeffs(6, layer) * x_old + coeffs(7, layer) * y_old + coeffs(8, layer);
+    
+    if (variant == 0) {
+      s = pow(x*x + y*y + z*z, 1/3);
+      x = x + sin(s);
+      y = y + cos(s);
+      z = z + s;
+
+    } else if (variant == 1) {
+      s = 1;
+      f = pi / 36; 
+      x = s * cos(x * f);
+      y = s * cos(y * f);
+      z = s * cos(z);
+
+    } else if (variant == 2) {
+      s = 2;
+      f = pi / 36; 
+      
+      x = s * sin(x * f);
+      y = s * sin(y * f);
+      z = s * sin(z);
+    }
+
+    // compute indices to be updated
+    x_ind = int (x * pixels * zoom) + pixels/2;
+    y_ind = int (y * pixels * zoom) + pixels/2;
+    
+    // wrap
+    while (x_ind < 0) {
+      x_ind = x_ind + pixels;
+    }
+    while (y_ind < 0) {
+      y_ind = y_ind + pixels;
+    }
+    while (x_ind >= pixels) {
+      x_ind = x_ind - pixels;
+    }
+    while (y_ind >= pixels) {
+      y_ind = y_ind - pixels;
+    }
+    
+    // store results if they fall within the range
+    if(x_ind >= 0 & x_ind < pixels) {
+      if(y_ind >= 0 & y_ind < pixels) {
+        count(x_ind, y_ind)++;
+        w = count(x_ind, y_ind) / (count(x_ind, y_ind) + 1); 
+        image(x_ind, y_ind) = (1-w) * z + w * image(x_ind, y_ind);
+      }
+    }
+    
+    // move new to old
+    x_old = x;
+    y_old = y;
+    z_old = z; 
+    
+  }
+
+  return image;
+}
+
+
